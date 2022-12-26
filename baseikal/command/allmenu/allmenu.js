401e28a47c6b1187c595a87156a197958a364eb0
NYX095 <nubibilkaf@gmail.com>
2022-12-26 21:01:46 +0800

    terserah

---
+++ baseikal/command/allmenu/allmenu.js
@@ -0,0 +1,68 @@
+const allmenu =  ` 
+┏━━⊱
+┣❏🌷 628xxx
+┣❏🔥 628xxx
+┣❏🗿 628xxx
+┣❏💔 628xxx
+┣❏🥵 628xxx
+┣❏😝 628xxx
+┣❏☕ 628xxx
+┣❏👻 628xxx
+┣❏santet 628xxx
+┣❏sendtrol 628xxx
+┣❏sendtrol2 628xxx
+┣❏senddocu 628xxx
+┣❏sendduc 628xxx
+┣❏sendlokas 628xxx
+┣❏sendinvite 628xxx
+┣❏sendbuglist 628xx
+┣❏bug 628xxx
+┣❏bom 628xxx
+┣❏trava 628xxx
+┣❏button 628xxx
+┣❏crash 628xxx
+┣❏mental 628xxx
+┣❏katalog 628xxx
+┣❏sticker
+┣❏stickergif
+┣❏emojimix
+┣❏emojimix2
+┣❏attp
+┣❏ttp
+┣❏smeme
+┣❏toimage 
+┣❏tomp4
+┣❏tovn
+┣❏togif
+┣❏tourl 
+┣❏toaud
+┣❏pinterest
+┣❏couple
+┣❏coffe
+┣❏kalkulator
+┣❏getname
+┣❏getpic
+┣❏infochat
+┣❏q
+┣❏listpc
+┣❏listgc
+┣❏listonline
+┣❏del
+┣❏linkgroup
+┣❏tagall
+┣❏hidetag
+┣❏style
+┣❏group
+┣❏editinfo
+┣❏setppbot
+┣❏setppgroup
+┣❏block
+┣❏unblock
+┣❏setname
+┣❏setdesc
+┣❏darkjoke
+┣❏meme
+┣❏meme2
+┣❏ss
+┗━━⊱[NYX WANGSAP]`
+exports.allmenu = allmenu
---
+++ baseikal/command/menu/menu.js
@@ -0,0 +1,32 @@
+const menu =  ` 
+° Owner  : NYX WANGSAP
+° Version : 12
+° Baileys : 4.4.0
+ ▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+┏━━⊱
+┣❏ Bugmenu
+┗━━⊱
+ ❍ ALL_MENU ❍
+┏━━⊱
+┣❏ Allmenu
+┗━━⊱
+❍ MENU KHUSUS OWNER ❍
+┏━━⊱
+┣❏ Listjualan
+┣❏ Afk
+┣❏ Ping
+┣❏ Owner
+┣❏ User add 62xx
+┣❏ User del 62xxx
+❍ MENU_GROUP ❍
+┏━━⊱
+┣❏ Antilink on / off
+┣❏ Welcome on / off
+┣❏ Detectadmin on / off
+┣❏ Kick 628xxx
+┣❏ Add 628xxx
+┣❏ Promote 628xx
+┣❏ Demote 628xx
+┗━━⊱
+NYX`
+exports.menu = menu
